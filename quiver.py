a1ebceb966e06b691a1665d5a012d509a4e51e77
Abdelhamid Laiadi <hamidlaiadi@gmail.com>
2025-05-19 13:57:06 +0100

    Update quiver.py

--- quiver.py
+++ quiver.py
@@ -3,6 +3,13 @@
 Created on Sun Feb 12 13:57:41 2023
 
 @author: HAMID
+
+This Python program generates a **quiver plot** using Matplotlib, where arrows represent
+a simple 2D vector field based on meshgrid coordinates from -10 to 10. Each arrow points
+from the origin of its grid cell in the direction defined by the grid itself 
+(i.e., the arrow at point $(x, y)$ points in the direction $(x, y)$).
+
+
 """
 
 import matplotlib.pyplot as plt
